5e1c140abab060ab9bf20c5cad4f409852a58f2e
nmjmdr <narasimha.gm@gmail.com>
2018-02-24 19:09:32 +1100

    Added serialized fetch, changed crawlers to use handlers map

--- tests/crawler.spec.js
+++ tests/crawler.spec.js
@@ -2,6 +2,7 @@ const chai = require('chai');
 const expect = chai.expect;
 const sinon = require('sinon');
 const lib = require('../lib/crawler');
+const eventNames = require('../lib/event-names');
 
 describe("Given Crawler",()=>{
   beforeEach(()=>{
@@ -15,21 +16,34 @@ describe("Given Crawler",()=>{
 
     const fetch = sinon.stub();
     const onPageResulted = sinon.stub();
-    const crawler = lib.getCrawler(fetch, onPageResulted);
+    const onAllPagesFetched = sinon.stub();
+    const onPageFetchErroed = sinon.stub();
+
+    let handlers = {};
+    handlers[eventNames.PageResulted] = onPageResulted;
+    handlers[eventNames.PageFetchErrored] = onPageFetchErroed,
+    handlers[eventNames.AllPagesFetched] = onAllPagesFetched
+
+    const crawler = lib.getCrawler(fetch, handlers);
+
     const nResults = 100;
     const keywords = "some keywords";
+    const resultsPerPage = 10;
 
     it("Should invoke fetcher with appropriate params",(done)=>{
 
-      crawler(nResults, keywords);
+      crawler(nResults, resultsPerPage, keywords);
 
       expect(fetch.called).to.be.true;
 
       const fetchArgs = fetch.getCalls(0)[0].args;
-      expect(fetchArgs[0]).to.be.equal(nResults);
-      expect(fetchArgs[1]).to.be.equal(keywords);
+      expect(fetchArgs[0]).to.be.deep.equal({
+        nResults,
+        resultsPerPage,
+        keywords
+      });
 
-      const resultsEmitter = fetchArgs[2];
+      const resultsEmitter = fetchArgs[1];
       expect(resultsEmitter).to.exist;
 
       done();
@@ -40,15 +54,44 @@ describe("Given Crawler",()=>{
       crawler(nResults, keywords);
 
       const fetchArgs = fetch.getCalls(0)[0].args;
-      const resultsEmitter = fetchArgs[2];
+      const resultsEmitter = fetchArgs[1];
       expect(resultsEmitter).to.exist;
 
       const page =  { "payload": "payload" };
 
-      resultsEmitter.emit(lib.PageResultedEvent, page);
+      resultsEmitter.emit(eventNames.PageResulted, page);
 
       expect(onPageResulted.called).to.be.true;
       expect(onPageResulted.getCalls(0)[0].args[0]).to.deep.equal(page);
+      done();
+    });
+
+    it("Should invoke onPageFetchErroed, whenever fetch results in an error",(done)=>{
+
+      crawler(nResults, keywords);
+
+      const fetchArgs = fetch.getCalls(0)[0].args;
+      const resultsEmitter = fetchArgs[1];
+      expect(resultsEmitter).to.exist;
+      const error = "error";
+      resultsEmitter.emit(eventNames.PageFetchErrored, error);
+
+      expect(onPageFetchErroed.called).to.be.true;
+      expect(onPageFetchErroed.getCalls(0)[0].args[0]).to.deep.equal(error);
+      done();
+    });
+
+    it("Should invoke onAllPagesFetched, whenever all pages are fetched",(done)=>{
+
+      crawler(nResults, keywords);
+
+      const fetchArgs = fetch.getCalls(0)[0].args;
+      const resultsEmitter = fetchArgs[1];
+      expect(resultsEmitter).to.exist;
+
+      resultsEmitter.emit(eventNames.AllPagesFetched);
+
+      expect(onAllPagesFetched.called).to.be.true;
       done();
     });
   });
