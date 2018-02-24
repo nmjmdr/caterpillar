f193ffb7d80444f1157fdc5c0829d2cb7862bbeb
nmjmdr <narasimha.gm@gmail.com>
2018-02-24 22:16:33 +1100

    Simplified crawler to only setup page fetched event handler and then invoke fetch

--- tests/crawler.spec.js
+++ tests/crawler.spec.js
@@ -4,86 +4,38 @@ const sinon = require('sinon');
 const lib = require('../lib/crawler');
 const events = require('../lib/event-names');
 
-describe('Given Crawler',()=>{
-  beforeEach(()=>{
-    sandbox = sinon.sandbox.create();
-  });
+describe.only('Given Crawler',()=>{
+  let sandbox = sinon.sandbox.create();
   afterEach(()=>{
     sandbox.restore();
   });
 
-  describe('When the crawler is setup',()=>{
-
-    const fetch = sinon.stub();
-    const onPageResulted = sinon.stub();
-    const onAllPagesFetched = sinon.stub();
-    const onPageFetchErroed = sinon.stub();
-
-    let handlers = {};
-    handlers[events.PageResulted] = onPageResulted;
-    handlers[events.PageFetchErrored] = onPageFetchErroed,
-    handlers[events.AllPagesFetched] = onAllPagesFetched
-
-    const crawler = lib.getCrawler(fetch, handlers);
-
+  describe('When crawler is setup',()=>{
+    const fetch = sandbox.stub();
+    const handler = sandbox.stub();
+    const crawl = lib.getCrawler(fetch, handler);
     const nResults = 100;
-    const keywords = 'some keywords';
     const resultsPerPage = 10;
+    const keywords = "some keywords";
 
-    beforeEach(()=>{
-      crawler(nResults, resultsPerPage, keywords);
-    })
-
-    it('Should invoke fetcher with appropriate params',(done)=>{
+    it('Should invoke the fetch to get all the pages',(done)=>{
+      crawl(nResults, resultsPerPage, keywords);
       expect(fetch.called).to.be.true;
-
-      const fetchArgs = fetch.getCalls(0)[0].args;
-      expect(fetchArgs[0]).to.be.deep.equal({
+      expect(fetch.getCalls(0)[0].args[0]).to.deep.equal({
         nResults,
         resultsPerPage,
         keywords
-      });
-
-      const resultsEmitter = fetchArgs[1];
-      expect(resultsEmitter).to.exist;
-
+      })
       done();
     });
 
-    it('Should invoke onPageResulted, whenever fetch gets a page',(done)=>{
-      const fetchArgs = fetch.getCalls(0)[0].args;
-      const resultsEmitter = fetchArgs[1];
-      expect(resultsEmitter).to.exist;
-
-      const page =  { 'payload': 'payload' };
-
-      resultsEmitter.emit(events.PageResulted, page);
-
-      expect(onPageResulted.called).to.be.true;
-      expect(onPageResulted.getCalls(0)[0].args[0]).to.deep.equal(page);
-      done();
-    });
-
-    it('Should invoke onPageFetchErroed, whenever fetch results in an error',(done)=>{
-      const fetchArgs = fetch.getCalls(0)[0].args;
-      const resultsEmitter = fetchArgs[1];
-      expect(resultsEmitter).to.exist;
-      const error = 'error';
-      resultsEmitter.emit(events.PageFetchErrored, error);
-
-      expect(onPageFetchErroed.called).to.be.true;
-      expect(onPageFetchErroed.getCalls(0)[0].args[0]).to.deep.equal(error);
-      done();
-    });
-
-    it('Should invoke onAllPagesFetched, whenever all pages are fetched',(done)=>{
-      const fetchArgs = fetch.getCalls(0)[0].args;
-      const resultsEmitter = fetchArgs[1];
-      expect(resultsEmitter).to.exist;
-
-      resultsEmitter.emit(events.AllPagesFetched);
-
-      expect(onAllPagesFetched.called).to.be.true;
+    it('Should subscribe the handler to PageFetched event',(done)=>{
+      crawl(nResults, resultsPerPage, keywords);
+      expect(fetch.called).to.be.true;
+      const emitter = fetch.getCalls(0)[0].args[1];
+      expect(emitter).to.exist;
+      emitter.emit(events.PageFetched);
+      expect(handler.called).to.be.true;
       done();
     });
   });
--- tests/serial-fetcher.spec.js
+++ tests/serial-fetcher.spec.js
@@ -7,18 +7,8 @@ const events = require('../lib/event-names');
 const pageFetcher = require('../lib/page-fetcher');
 
 describe('Given the serial fetcher',()=>{
-  beforeEach(()=>{
-    sandbox = sinon.sandbox.create();
-  });
+  let sandbox = sinon.sandbox.create();
   afterEach(()=>{
     sandbox.restore();
-  });
-
-  describe('When fetch is invoked',()=>{
-
-    const pageFetch = sinon.stub();
-    const pageFetchCreate = sinon.stub(pageFetcher, 'create').returns(pageFetch);
-    class ResultsEmitter extends EventEmitter {};
-
   });
 });
